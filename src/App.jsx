c9ecb565425f8a49f0b4559af12f496cd36a137e
Juan Felipe Muñoz Hurtado <juanfelipemh@gmail.com>
2022-11-04 18:55:40 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -37,14 +37,14 @@ function App() {
           {/* Rutas Publicas */}
           <Routes>
             <Route path="/" element={<Layout />}>       
-            <Route index element={<Inicio />} />       
+            <Route path="inicio" index element={<Inicio />} />       
               <Route path="registrar" element={<Registrar />} />
               <Route path="confirmar/:token" element={<ConfirmarCuenta />} />
             </Route>
 
             {/* Rutas Protegidas */}
             <Route path="/" element={<RutaProtegida />}>
-            <Route element={<AdministrarUsuarios/>} />
+            <Route path="administrar" element={<AdministrarUsuarios/>} />
               <Route path="modificar/:id" element={<ModificarProd />} />              
               <Route path="perfil" element={<EditarPerfil/>} />            
               <Route path="cambiar-password" element={<CambiarPassword/>} />
